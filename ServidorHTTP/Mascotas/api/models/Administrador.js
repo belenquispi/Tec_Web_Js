35009fdfdbe707252e32f2105227e758c2ece492
belenquispi <belen.quispi@epn.edu.ec>
2016-12-21 13:30:21 -0500

    Creación del modelo Administrador

---
+++ ServidorHTTP/Mascotas/api/models/Administrador.js
@@ -0,0 +1,32 @@
+/**
+ * Administrador.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      nombre:{
+          type:'string',
+          minLength: 5
+      },
+      apellido:{
+          type:'string',
+          minLength: 5
+      },
+      email:{
+          type:'string',
+          email:true,
+          defaultsTo: 'correo@valido.com'
+      },
+      tipo:{
+          type:'string',
+          enum:['Perfil','Usuarios','Correos'],
+          defaulsTo:'Usuarios'
+      }
+
+  }
+};
+
